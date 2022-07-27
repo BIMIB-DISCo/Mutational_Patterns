db6746be045058b891901783551dbd6467675d70
Gianluca Ascolani <64789019+gian-asco@users.noreply.github.com>
2022-07-27 11:47:17 +0200

    Add files via upload
    denovo 5 context signatures projected to 3 context signatures are different from denovo 3 context signatures

--- MutationalPatterns_ 5_3.R
+++ MutationalPatterns_ 5_3.R
@@ -1,3 +1,4 @@
+#####
 ### MutationalPatterns_.R
 
 library(MutationalPatterns)
@@ -12,11 +13,13 @@ library(gplots)
 
 ref_genome <- available.genomes()[59]
 ref_genome <- available.genomes()[52] #64
+ref_genome <- available.genomes()[60] #64
+ref_genome <- "BSgenome.Hsapiens.NCBI.GRCh38"
 print(ref_genome)
 
-if(ref_genome %in% rownames(installed.packages()) == FALSE) 
+if(ref_genome %in% rownames(installed.packages()) == FALSE)
 {
-	BiocManager::install(ref_genome, update = FALSE)
+  BiocManager::install(ref_genome, update = FALSE)
 }
 library(ref_genome, character.only = TRUE)
 
@@ -37,7 +40,7 @@ compress_context <- function(signature, flanking = 1) {
   colnames(df) <- colnames(signature)
   for (s in triseq)
   {
-    str_pat <- paste0(".*",s,".*") 
+    str_pat <- paste0(".*",s,".*")
     pat <- str_pat
     pat <- str_replace_all(pat,"\\[","\\\\[")
     pat <- str_replace_all(pat,"\\]","\\\\]")
@@ -71,7 +74,7 @@ sample_names <- c(vcf_files_20, vcf_files_par, vcf_files_treated)
 sample_names <- str_replace_all(str_replace_all(sample_names, ".*test.",""), "\\.+full|^full.|.vcf$", "")
 sample_names
 
-tissue <- c(rep("colon", length(c(vcf_files_20,vcf_files_par))), 
+tissue <- c(rep("colon", length(c(vcf_files_20,vcf_files_par))),
             rep("colon_treated", length(c(vcf_files_treated))))
 
 
@@ -112,16 +115,17 @@ i=0
 p=plot_spectrum(type_occurrences)
 pdf(paste0(fig_folder, i, ".pdf"), height=15, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-p=plot_spectrum(type_occurrences, CT = FALSE, 
-              indv_points = TRUE, legend = TRUE)
+p=plot_spectrum(type_occurrences, CT = FALSE,
+                indv_points = TRUE, legend = TRUE)
 pdf(paste0(fig_folder, i, ".pdf"), height=15, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
+
 
 
 mut_mat_3 <- mut_matrix(snv_grl, ref_genome, extension = 1)
@@ -132,31 +136,94 @@ p=plot_profile_heatmap(mut_mat_3, by = sample_names)
 
 pdf(paste0(fig_folder, i, ".pdf"), height=65, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 p=plot_96_profile(mut_mat_3)
 pdf(paste0(fig_folder, i, ".pdf"), height=55, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
 mut_mat_5 <- mut_matrix(snv_grl, ref_genome, extension = 2)
 head(mut_mat_5)
 
+####
+vcf_list = snv_grl
+extension = 2
+
+if (inherits(vcf_list, "list")) {
+  vcf_list <- GenomicRanges::GRangesList(vcf_list)
+}
+if (inherits(vcf_list, "CompressedGRangesList")) {
+  gr_sizes <- S4Vectors::elementNROWS(vcf_list)
+  gr <- BiocGenerics::unlist(vcf_list)
+} else if (inherits(vcf_list, "GRanges")) {
+  gr <- vcf_list
+  gr_sizes <- length(gr)
+  names(gr_sizes) <- "My_sample"
+} else {
+  .not_gr_or_grl(vcf_list)
+}
+type_context <- type_context(gr, ref_genome, extension)
+
+##--
+gr_sizes
+categories <- count <- NULL
+nr_bases <- nchar(type_context$context[[1]])
+middle_base <- ceiling(nr_bases/2)
+bases_left <- c("A", "C", "G", "T")
+bases_right <- c("A", "C", "G", "T")
+base_subs <- c("[C>A]", "[C>G]", "[C>T]", "[T>A]", "[T>C]", 
+               "[T>G]")
+full_context_poss <- vector("list", length(base_subs))
+for (i in seq_along(base_subs)) {
+  sub <- base_subs[[i]]
+  sub_context <- sub
+  for (j in seq_len(middle_base - 1)) {
+    combi_tb <- tidyr::crossing(bases_left, sub_context, 
+                                bases_right)
+    sub_context <- paste0(combi_tb$bases_left, combi_tb$sub_context, 
+                          bases_right)
+  }
+  full_context_poss[[i]] <- sub_context
+}
+full_context_poss <- do.call(c, full_context_poss)
+full_context <- stringr::str_c(substr(type_context$context, 
+                                      1, middle_base - 1), "[", type_context$types, "]", substr(type_context$context, 
+                                                                                                middle_base + 1, nr_bases)) %>% factor(levels = full_context_poss)
+if (is.null(names(gr_sizes))) {
+  names(gr_sizes) <- seq_along(gr_sizes)
+}
+sample_vector <- rep(names(gr_sizes), gr_sizes) %>% factor(levels = names(gr_sizes))
+counts <- tibble::tibble(categories = full_context, sample = sample_vector) %>% 
+  dplyr::filter(!is.na(categories)) %>% dplyr::group_by(categories, 
+                                                        sample, .drop = FALSE) %>% dplyr::summarise(count = dplyr::n())
+counts <- tidyr::spread(counts, key = sample, value = count, 
+                        fill = 0)
+unnecesary_cols <- which(colnames(counts) == "<NA>")
+mut_mat <- as.matrix(counts[, -c(1, unnecesary_cols)])
+rownames(mut_mat) <- counts$categories
+#return(mut_mat)
+
+
+
+####
+
+
 plot_profile_heatmap(mut_mat_5, by = sample_names)
 p=plot_profile_heatmap(mut_mat_5, by = sample_names)
 pdf(paste0(fig_folder, i, ".pdf"), height=65, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
 p=plot_profile_heatmap(mut_mat_5, by = tissue)
 pdf(paste0(fig_folder, i, ".pdf"), height=6, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -166,12 +233,10 @@ print(sum(mut_mat_5_to_3-mut_mat_3))
 p=plot_96_profile(mut_mat_5_to_3)
 pdf(paste0(fig_folder, i, ".pdf"), height=55, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-
-mut_mat2 <- mut_mat_5 #+ 0.0001 #operation already in the code
 
 
 estimate_5 <- nmf(mut_mat_5, rank = 2:4, method = "brunet", nrun = 200, seed = 123456, .opt = "v-p")
@@ -180,7 +245,7 @@ plot(estimate_5)
 p=plot(estimate_5)
 pdf(paste0(fig_folder, i, ".pdf"), height=5, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -188,8 +253,9 @@ signatures = get_known_signatures()
 
 
 # 3 denovo signatures using 5 bases
-nmf_res_5_3S <- extract_signatures(mut_mat_5, rank = 3 , nrun = 200, single_core = TRUE)
-colnames(nmf_res_5_3S$signatures) <- paste("Signature",seq(1:3))
+rank = 3
+nmf_res_5_3S <- extract_signatures(mut_mat_5, rank = rank , nrun = 200, single_core = TRUE)
+colnames(nmf_res_5_3S$signatures) <- paste("Signature",seq(1:rank))
 rownames(nmf_res_5_3S$contribution) <- colnames(nmf_res_5_3S$signatures)
 
 
@@ -197,46 +263,97 @@ rownames(nmf_res_5_3S$contribution) <- colnames(nmf_res_5_3S$signatures)
 p=plot_profile_heatmap(nmf_res_5_3S$signatures)
 pdf(paste0(fig_folder, i, ".pdf"), height=6, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-nmf_res_5_3S_to_3 <- compress_context(nmf_res_5_3S$signatures, flanking = 1)
-nmf_res_5_3S_to_3_rec <- compress_context(nmf_res_5_3S$reconstructed, flanking = 1)
+nmf_res_5_3S_to_3 <- nmf_res_5_3S
+nmf_res_5_3S_to_3$signatures <- compress_context(nmf_res_5_3S$signatures, flanking = 1)
+nmf_res_5_3S_to_3$reconstructed <- compress_context(nmf_res_5_3S$reconstructed, flanking = 1)
 
-p=plot_96_profile(nmf_res_5_3S_to_3)
+p=plot_96_profile(nmf_res_5_3S_to_3$signatures)
 pdf(paste0(fig_folder, i, ".pdf"), height=55, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-p=plot_original_vs_reconstructed(mut_mat_5, nmf_res_5_3S$reconstructed, 
+#p=plot_original_vs_reconstructed(mut_mat_3, nmf_res_3$reconstructed, y_intercept = 0.95)
+
+p=plot_original_vs_reconstructed(mut_mat_5, nmf_res_5_3S$reconstructed,
                                  y_intercept = 0.95)
 pdf(paste0(fig_folder, i, ".pdf"), height=7, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
 
-
-p=plot_original_vs_reconstructed(nmf_res_5_3S_to_3, nmf_res_5_3S_to_3_rec, 
+p=plot_original_vs_reconstructed(mut_mat_5_to_3, nmf_res_5_3S_to_3$reconstructed,
                                  y_intercept = 0.95)
 pdf(paste0(fig_folder, i, ".pdf"), height=7, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
+
+
+
+
 
 #should this be done for each sample?
 plot_compare_profiles(as.matrix(mut_mat_5_to_3)[, 1],
-                      nmf_res_5_3S_to_3_rec[, 1],
+                      nmf_res_5_3S_to_3$reconstructed[, 1],
                       profile_names = c("Original", "Reconstructed"),
                       #condensed = TRUE
 )
 
 
-
-rename_nmf_signatures(nmf_res_5_3S_to_3, signatures, cutoff = 0.85)
+signatures = get_known_signatures()
+names(rename_nmf_signatures(nmf_res_5_3S_to_3, signatures, cutoff = 0.80)$signatures)
 colnames(nmf_res$signatures)
+
+
+
+
+rank = 3
+nmf_res_3_3S <- extract_signatures(mut_mat_3, rank = rank , nrun = 200, single_core = TRUE)
+colnames(nmf_res_3_3S$signatures) <- paste("Signature",seq(1:rank))
+rownames(nmf_res_3_3S$contribution) <- colnames(nmf_res_3_3S$signatures)
+
+#first main results of signature context
+sum(nmf_res_5_3S_to_3$reconstructed - nmf_res_3_3S$reconstructed)
+
+
+
+fit_cosmic_to_5_3 <- fit_to_signatures(mut_mat_5_to_3, signatures)
+
+plot_contribution(fit_cosmic_to_5_3$contribution,
+                  coord_flip = FALSE,
+                  mode = "relative"
+)
+
+
+p=plot_contribution(fit_res_merged$contribution,
+                    coord_flip = FALSE,
+                    mode = "relative"
+)
+pdf(paste0("./fig/", i, ".pdf"), height=7, width=8.8)
+p
+dev.off()
+i=i+1
+
+fit_cosmic_to_3 <- fit_to_signatures(mut_mat_3, signatures)
+plot_contribution(fit_cosmic_to_3$contribution,
+                  coord_flip = FALSE,
+                  mode = "relative"
+)
+
+fit_cosmic_to_3$contribution / fit_cosmic_to_5_3$contribution
+
+mut_mat_5_to_3 -  mut_mat_3
+
+
+
+
+
 
 # 2 denovo signatures using 5 bases
 nmf_res_5_2S <- extract_signatures(mut_mat_5, rank = 2 , nrun = 200, single_core = TRUE)
@@ -248,7 +365,7 @@ rownames(nmf_res_5_2S$contribution) <- colnames(nmf_res_5_2S$signatures)
 p=plot_profile_heatmap(nmf_res_5_2S$signatures)
 pdf(paste0(fig_folder, i, ".pdf"), height=6, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -257,7 +374,7 @@ nmf_res_5_2S_to_3 <- compress_context(nmf_res_5_2S$signatures, flanking = 1)
 p=plot_96_profile(nmf_res_5_2S_to_3)
 pdf(paste0(fig_folder, i, ".pdf"), height=55, width=17)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 ####
@@ -265,12 +382,12 @@ i=i+1
 
 
 
-p=plot_contribution_heatmap(nmf_res$contribution, 
-                          cluster_samples = TRUE, 
-                          cluster_sigs = TRUE)
+p=plot_contribution_heatmap(nmf_res$contribution,
+                            cluster_samples = TRUE,
+                            cluster_sigs = TRUE)
 pdf(paste0("./fig/", i, ".pdf"), height=6.6, width=7)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -284,38 +401,38 @@ samples_order <- colnames(mut_mat2)[hclust_samples$order]
 samples_order
 
 p=plot_contribution_heatmap(nmf_res$contribution,
-                          sig_order = signatures_order, sample_order = samples_order,
-                          cluster_sigs = FALSE, cluster_samples = FALSE
+                            sig_order = signatures_order, sample_order = samples_order,
+                            cluster_sigs = FALSE, cluster_samples = FALSE
 )
 pdf(paste0("./fig/", i, ".pdf"), height=10, width=12)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-p=plot_original_vs_reconstructed(mut_mat2, nmf_res$reconstructed, 
-                               y_intercept = 0.95)
+p=plot_original_vs_reconstructed(mut_mat2, nmf_res$reconstructed,
+                                 y_intercept = 0.95)
 pdf(paste0("./fig/", i, ".pdf"), height=7, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
 cos_sim_signatures <- cos_sim_matrix(nmf_res$signatures, nmf_res$signatures)
-p=plot_cosine_heatmap(cos_sim_signatures, 
-                    cluster_rows = TRUE, cluster_cols = TRUE)
+p=plot_cosine_heatmap(cos_sim_signatures,
+                      cluster_rows = TRUE, cluster_cols = TRUE)
 pdf(paste0("./fig/", i, ".pdf"), height=5, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
 
-p=plot_cosine_heatmap(cos_sim_matrix(mut_mat2, mut_mat2), 
-                    cluster_rows = TRUE, cluster_cols = TRUE)
+p=plot_cosine_heatmap(cos_sim_matrix(mut_mat2, mut_mat2),
+                      cluster_rows = TRUE, cluster_cols = TRUE)
 pdf(paste0("./fig/", i, ".pdf"), height=10, width=12)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -330,11 +447,11 @@ merged_signatures <- merge_signatures(nmf_res$signatures, cos_sim_cutoff = 0.82)
 #merged_signature <- round(merged_signatures)
 colnames(merged_signatures)
 
-p=plot_cosine_heatmap(cos_sim_matrix(merged_signatures, merged_signatures), 
-                    cluster_rows = TRUE, cluster_cols = TRUE)
+p=plot_cosine_heatmap(cos_sim_matrix(merged_signatures, merged_signatures),
+                      cluster_rows = TRUE, cluster_cols = TRUE)
 pdf(paste0("./fig/", i, ".pdf"), height=10, width=12)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
@@ -342,19 +459,19 @@ fit_res_merged <- fit_to_signatures(mut_mat2, merged_signatures)
 
 
 p=plot_contribution(fit_res_merged$contribution,
-                  coord_flip = FALSE,
-                  mode = "relative"
+                    coord_flip = FALSE,
+                    mode = "relative"
 )
 pdf(paste0("./fig/", i, ".pdf"), height=7, width=8.8)
 p
-dev.off() 
+dev.off()
 i=i+1
 
-p=plot_original_vs_reconstructed(mut_mat2, fit_res_merged$reconstructed, 
-                               y_intercept = 0.95)
+p=plot_original_vs_reconstructed(mut_mat2, fit_res_merged$reconstructed,
+                                 y_intercept = 0.95)
 pdf(paste0("./fig/", i, ".pdf"), height=7, width=5)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 merged_sig_name <- paste("signature_m",seq(1:length(colnames(merged_signatures))))
@@ -375,22 +492,22 @@ names(rename_nmf_signatures(fit_res_merged_reduced, signatures, cutoff = 0.87)$s
 
 
 
-p=plot_contribution_heatmap(fit_res_merged_std$contribution, 
-                            cluster_samples = TRUE, 
+p=plot_contribution_heatmap(fit_res_merged_std$contribution,
+                            cluster_samples = TRUE,
                             cluster_sigs = TRUE)
 pdf(paste0("./fig/", i, ".pdf"), height=6.6, width=5)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
 p=plot_profile_heatmap(as.matrix.data.frame(fit_res_merged_std$signatures), by=merged_sig_name)
 pdf(paste0("./fig/", i, ".pdf"), height=10, width=12)
 p
-dev.off() 
+dev.off()
 i=i+1
 
-contri_boots <- fit_to_signatures_bootstrapped(mut_mat2, 
+contri_boots <- fit_to_signatures_bootstrapped(mut_mat2,
                                                as.matrix.data.frame(fit_res_merged_std$signature),
                                                n_boots = 150,
                                                method = "strict"
@@ -399,16 +516,16 @@ contri_boots <- fit_to_signatures_bootstrapped(mut_mat2,
 p=plot_bootstrapped_contribution(contri_boots, plot_type = "barplot")
 pdf(paste0("./fig/", i, ".pdf"), height=35, width=3)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
-p=plot_bootstrapped_contribution(contri_boots, 
-                               mode = "relative", 
-                               plot_type = "dotplot")
+p=plot_bootstrapped_contribution(contri_boots,
+                                 mode = "relative",
+                                 plot_type = "dotplot")
 pdf(paste0("./fig/", i, ".pdf"), height=12, width=6)
 p
-dev.off() 
+dev.off()
 i=i+1
 
 
